dfd6399b8fc6a9bd918e72762aa4fe9dfbb1b6e9
kailash1198 <kailash414243@gmail.com>
2021-09-05 07:13:31 +0530

    file/folder upload on repo

---
+++ javascript/script.js
@@ -0,0 +1,246 @@
+
+// VARIABLES IN JAVASCRIPT
+// three types of variables 
+// var
+// let
+// const
+
+// var kailash='aryan'
+// aryan='kailash'
+// console.log(aryan)
+
+
+// let kailash='aryan'
+// aryan='kailash'
+// let kailash='laxmi'
+// console.log(kailash)
+
+
+
+// const kailash ='aryan'
+// // kailash='laxmi'
+// const kailash='hjggds'
+// console.log(kailash)
+
+
+
+
+
+
+//DATA TYPES IN JAVASCRIPT-
+
+//string data types
+// var name='kailash'
+// console.log(name)
+// console.log(typeof name)
+
+
+//number data types
+// var num1=20
+// console.log(num1)
+// console.log(typeof num1)
+
+
+
+// boolean data types 
+// var exampleBoolean=true
+// console.log(exampleBoolean);
+// console.log(typeof exampleBoolean);
+
+//undefind data types 
+// var number1=
+// console.log(typeof number1);
+
+
+// null data types
+// var number2=null
+// console.log(number2);
+// console.log(typeof number2);
+
+
+//array data types
+// var kailash = ['kailash']
+// console.log(typeof kailash);
+
+
+
+// // object data types 
+// var aryan = {kailash:'aryan'}
+// console.log(typeof aryan)
+
+
+
+
+
+
+
+              // OPERATORS IN JAVASCRIPT-
+
+                //airthmatic operators
+
+// // addition operator (+)
+// var a=20;
+// var b=20;
+// var c=a+b;
+// console.log(c);
+
+// // subtraction operator (-)
+// var a=200;
+// var b=40;
+// var c = a-b;
+// console.log(c);
+
+// // Multiplication operator (*)
+// var a=10;
+// var b= 20;
+// var c = a*b;
+// console.log(c);
+
+// // division operator (/) 
+// var a=20;
+// var b=2;
+// var c = a/b;
+// console.log(c);
+
+// // modulus operator (%)
+// var a = 20;
+// var b = 10;
+// var c =a%b;
+// console.log(c);
+
+// // var p=100;
+// // var q =50;
+// // var s=p%q;
+// // console.log(s);
+
+// // var a =1000;
+// // var b=2000;
+// // var c=a%b;
+// // console.log(c);
+
+// // var a=10;
+// // var b=5;
+// // var c =a**b;
+// // console.log(c);
+
+
+// // var a=10;
+// // var b=5;
+// // var c=a-b;
+// // // console.log(c);
+// // a--;
+// // console.log(a-b);
+
+                      // // ASSIGNMENT OPERATORS 
+
+// var a=10
+// var b=20
+// a+=b
+// console.log(a)
+
+// var a=20
+// var b=5
+// a-=b
+// console.log(a)
+
+// var a=30
+// var b=2
+// a/=b
+// console.log(a)
+
+// var a=30
+// var b=20
+// a*=b
+// console.log(a)
+
+// var a=20
+// var b=3
+// a%=b
+// console.log(a)
+
+// var x=5
+// var y=2
+// x**=y
+// console.log(x)
+
+/*var x=212
+var y=8
+x%=y
+console.log(x)*/
+
+
+       //comparison oparators
+
+    //    result true or false
+
+    // var a=10
+    // var b=10
+    // console.log(a==b)
+
+    // var a=20
+    // var b=10
+    // console.log(a==b)
+
+    // var a=20
+    // var b=20
+    // console.log(a===b)
+
+    // var a=30
+    // var b=10
+    // console.log(a===b)
+
+// var a=10
+// var b=20
+// console.log(a!=b)
+
+// var a=30
+// var b=30
+// console.log(a!=b)
+
+// var a=10
+// var b=20
+// console.log(a!==b)
+
+// var a=30
+// var b=20
+// console.log(a>b)
+
+// var a=20
+// var b=50
+// console.log(a<b)
+
+// var a=40
+// var b=20
+// console.log(a>=b)
+
+// var a=20
+// var b=50
+// console.log(a<=b)
+
+
+
+
+
+
+    //  IF STATEMENT 
+
+// var a=20
+// var b=100
+// var c=a+b
+// if (a>b) {
+//   console.log(c)
+// }
+
+// let a=100
+// let b=100
+// let c=a+b
+// if (a==b){
+//   console.log(c);
+// }
+
+// var a=100
+// var b=20
+// var c=a-b
+// if (a>b) {
+//   console.log(c)
+// }
